6d4121d61d76eeda7bd8ba53fa620488643a2e43
mdw13423 <mwolvert@ucsc.edu>
2021-04-21 23:41:35 -0700

    Fixed folder names for publication

--- Src/scenes/Play.js
+++ Src/scenes/Play.js
@@ -6,11 +6,11 @@ class Play extends Phaser.Scene {
     preload() {
         // load images/tile sprites
         console.log("Play.preload() is running");
-        this.load.image('rocket', './assets/rocket.png');
-        this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('rocket', './Assets/rocket.png');
+        this.load.image('spaceship', './Assets/spaceship.png');
+        this.load.image('starfield', './Assets/starfield.png');
         // load spritesheet
-        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('explosion', './Assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
       }
 
     create() {
